f8c847b693c12afdb187810b642e39cd51318714
Eric Burel <e.burel.contact@gmail.com>
2016-04-19 15:42:26 +0200

    Added test and coverage shell scripts

---
+++ coverage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# @see https://blog.golang.org/cover
+# Generate the coverage report
+instrument(){
+	go test -covermode=count -coverprofile=validation/"$1".out "./$1"
+}
+# Print coverage report as a nice html
+cover(){
+	go tool cover -html=validation/"$1".out
+}
+instrument event
+instrument person
+instrument habit
+# generate report
+cover event
+cover person
+cover habit
