83187dee95e40536cb4ddfb7c0f227ae62c73ada
Eric Burel <e.burel.contact@gmail.com>
2016-04-19 19:01:36 +0200

    Fixed timetogo so that it can handle float64 and float32
    The times function allow to multiply any kind of number to a duration, and
    returns a duration
    It needs hard coded cast, since the Duration itself needs to be casted to
    a float type
    Not every case is handled (uint and so on)

--- timetogo/timetogo.go
+++ timetogo/timetogo.go
@@ -2,31 +2,79 @@ package timetogo
 
 import "time"
 
-func duration(i int) (d time.Duration) {
-	d = time.Duration(i)
+// Durationer An object that can be converted to a duration
+type Durationer interface {
+	Duration() time.Duration
+}
+
+func times(v interface{}, d time.Duration) (res time.Duration) {
+	// we check if v is a number and do the correct cast depending on results
+	// TODO : this should be far more complete, maybe using a loop on all possible numerical type
+	i, isInt := v.(int)
+	if isInt {
+		res = time.Duration(i) * d
+		return
+	}
+	f64, isFloat64 := v.(float64)
+	if isFloat64 {
+		// multiply float by the duration
+		product := f64 * float64(d)
+		// cast product to an int (= number of nanosecond), then to a duration
+		res = time.Duration(int(product))
+		return
+	}
+	f32, isFloat32 := v.(float32)
+	if isFloat32 {
+		// multiply float by the duration
+		product := f32 * float32(d)
+		res = time.Duration(int(product))
+		return
+	}
+	// TODO add panic return
+	return
+}
+
+// Duration Converts an int to a duration (number of nanoseconds)
+func Duration(v interface{}) (d time.Duration) {
+	i, isInt := v.(int)
+	if isInt {
+		d = time.Duration(i)
+		return
+	}
+	f64, isFloat64 := v.(float64)
+	if isFloat64 {
+		d = time.Duration(int(f64))
+		return
+	}
+	f32, isFloat32 := v.(float32)
+	if isFloat32 {
+		d = time.Duration(int(f32))
+		return
+	}
 	return
 }
 
 // Seconds Converts an value value in minutes
-func Seconds(i int) (d time.Duration) {
-	d = duration(i) * time.Second
+func Seconds(v interface{}) (d time.Duration) {
+	d = times(v, time.Second)
 	return
 }
 
 // Minutes Converts an value value in minutes
-func Minutes(i int) (d time.Duration) {
-	d = duration(i) * time.Minute
+func Minutes(v interface{}) (d time.Duration) {
+	d = times(v, time.Minute)
 	return
 }
 
 // Hours Converts an value value in minutes
-func Hours(i int) (d time.Duration) {
-	d = duration(i) * time.Hour
+func Hours(v interface{}) (d time.Duration) {
+	d = times(v, time.Hour)
 	return
 }
 
 // Days Converts an value value in minutes
-func Days(i int) (d time.Duration) {
-	d = duration(i) * time.Hour * 24
+func Days(v interface{}) (d time.Duration) {
+	// there are no unified definition of a day, so this is an approx
+	d = times(v, time.Hour) * 24
 	return
 }
--- timetogo/timetogo_test.go
+++ timetogo/timetogo_test.go
@@ -1,6 +1,7 @@
 package timetogo
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -9,12 +10,16 @@ import (
 
 func TestSecond(t *testing.T) {
 	assert.Equal(t, time.Duration(2)*time.Second, Seconds(2))
+	// in nanoseconds
+	assert.Equal(t, time.Duration(25*int(math.Pow10(8))), Seconds(2.5))
 }
 func TestMinute(t *testing.T) {
 	assert.Equal(t, time.Duration(2)*time.Minute, Minutes(2))
+	assert.Equal(t, time.Duration(2500000000*60), Minutes(2.5))
 }
 func TestHour(t *testing.T) {
 	assert.Equal(t, time.Duration(2)*time.Hour, Hours(2))
+	assert.Equal(t, time.Duration(2500000000*60*60), Hours(2.5))
 }
 func TestDay(t *testing.T) {
 	assert.Equal(t, time.Duration(2)*time.Hour*24, Days(2))
